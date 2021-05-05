66269273e5abe9b86d9f26fd9c4f534e1e94f60d
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-05-05 15:42:12 +0300

    removed unnecessary methods

--- src/main/scala/ru/ifmo/backend_2021/connections/ConnectionPool.scala
+++ src/main/scala/ru/ifmo/backend_2021/connections/ConnectionPool.scala
@@ -1,14 +1,12 @@
 package ru.ifmo.backend_2021.connections
 
-import cask.{WsActor, WsChannelActor}
+import cask.WsChannelActor
 import cask.endpoints.WsHandler
 import cask.util.Logger
 import cask.util.Ws.Event
 
 trait ConnectionPool {
-  def getConnections: List[WsChannelActor]
   def send(event: Event): WsChannelActor => Unit
   def sendAll(event: Event): Unit
-  def addConnection(connections: WsChannelActor)(implicit ac: castor.Context, log: Logger): WsActor
   def wsHandler(onConnect: WsChannelActor => Unit)(implicit ac: castor.Context, log: Logger): WsHandler
 }
